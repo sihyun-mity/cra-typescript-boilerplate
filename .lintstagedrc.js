efa56c3d6c34ae67ff2a3bf46aa2fc896408c722
Sihyeon Hong <72115242+sihyun-mity@users.noreply.github.com>
2024-01-04 18:04:06 +0900

    feat: pre-commit test 기능 추가

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,9 @@
 // See https://nextjs.org/docs/basic-features/eslint#lint-staged for details
 
+const prettier = 'prettier --write';
+const eslint = 'eslint --fix';
+
 module.exports = {
-  '**/*.{js,jsx,ts,tsx,html}': ['prettier --write', 'eslint --fix'],
-  '**/*.{css,scss,json}': 'prettier --write',
+  '**/!(*.test).{js,jsx,ts,tsx,html}': [prettier, eslint],
+  '**/*.{css,scss,json}': prettier,
 };
