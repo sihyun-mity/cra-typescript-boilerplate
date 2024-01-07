c0c6e4bee3b138d95bd6f6c3b0493555a11d25a6
Sihyeon Hong <72115242+sihyun-mity@users.noreply.github.com>
2024-01-08 00:55:42 +0900

    refactor: shuffle 테스트 단순화

--- src/utils/shuffle/shuffle.test.ts
+++ src/utils/shuffle/shuffle.test.ts
@@ -4,7 +4,7 @@ describe('Util: shuffle', () => {
   it('shuffle numbers', () => {
     const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     const shuffledArr = [...arr];
-    for (let i = 0; i < 100; i++) shuffle(shuffledArr);
+    shuffle(shuffledArr);
     expect(shuffledArr).not.toEqual(arr);
   });
 });
