80d0258040e8671c523d7e79995b42844cfd2d76
Sihyeon Hong <72115242+sihyun-mity@users.noreply.github.com>
2024-01-08 01:00:42 +0900

    chore: HTML ESLint 검사 제외

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -4,6 +4,6 @@ const prettier = 'prettier --write';
 const eslint = 'eslint --fix';
 
 module.exports = {
-  '**/!(*.test).{js,jsx,ts,tsx,html}': [prettier, eslint],
-  '**/*.{css,scss,json}': prettier,
+  '**/!(*.test).{js,jsx,ts,tsx}': [prettier, eslint],
+  '**/*.{css,scss,json,html}': prettier,
 };
