c993ea8cb66fb5f073d11961f3e2a38d0fb263b6
Sihyeon Hong <72115242+sihyun-mity@users.noreply.github.com>
2024-05-28 14:06:02 +0900

    refac: optimize event 삭제

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,4 +2,3 @@ export * from './elapsed-time';
 export * from './scroll';
 export * from './shuffle';
 export { default as http } from './http';
-export { default as optimizeEvent } from './optimize-event';
