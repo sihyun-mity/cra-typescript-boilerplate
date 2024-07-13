29121eb8e953c82692863ab8fd56126d32626286
Sihyeon Hong <72115242+sihyun-mity@users.noreply.github.com>
2024-07-13 14:48:23 +0900

    feat: react helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from '@/pages';
+import { HelmetProvider } from 'react-helmet-async';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 
